1b2650d1c78435e003410cb483fdc06b7a17c2c8
Kaakaanuts <nayaka_bintang_pratama_rpl2023@smkn1-sby.sch.id>
2025-12-30 15:10:43 +0700

    fix firebase kehapus

---
+++ resources/js/libs/firebase.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+import { getAuth, signInAnonymously } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3LX1BgAv80_dxgm7rMkXYMJKde7ataSo",
+  authDomain: "chat-mti-88.firebaseapp.com",
+  databaseURL: "https://chat-mti-88-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-mti-88",
+  storageBucket: "chat-mti-88.firebasestorage.app",
+  messagingSenderId: "259515011828",
+  appId: "1:259515011828:web:2f7ef40add69064d0ac4a1",
+  measurementId: "G-0PXW8RZWLG"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getDatabase(app);
+const auth = getAuth(app);
+signInAnonymously(auth)
+  .then(() => {
+    console.log('Firebase Auth: Signed in anonymously');
+  })
+  .catch((error) => {
+    console.error('Firebase Auth Error:', error);
+  });
+const storage = getStorage(app);
+export { db, auth, storage };
